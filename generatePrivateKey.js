0bc6f97a97ff532ccebf175a2bd7404d346f6008
Dastageercz <dastageer.sayed@codezeros.com>
2022-07-13 16:13:21 +0530

    Receiving BTC from Faucet

--- generatePrivateKey.js
+++ generatePrivateKey.js
@@ -1,26 +1,75 @@
-// import * as bip39 from "@scure/bip39";
-import bip39 from 'bip39'
-//import pbkdf2 from 'crypto-pbkdf2-hmac';
-//import sha256 from "sha256";
-import HDKey from "hdkey";
-// import { wordlist } from "@scure/bip39/wordlists/english.js";
+let express = require('express');
+let bip39 = require('bip39');
+let HDKey = require('hdkey');
+const { validate, getAddressInfo } = require('bitcoin-address-validation')
+let bitcoin = require('bitcoinjs-lib');
 
+let BIP32Factory = require('bip32').default
 
+var pubKey, privKey, address;
+const router = express.Router();
 
-const mnemonic = bip39.generateMnemonic()
-console.log("Seed:", mnemonic);
-// const mnemonicPhase = mnemonic.toString();
+/*-------------------------------------------------Logic---------------------------------------------*/
 
-// const seedPhase = mnemonicPhase;
-// const password = seedPhase;
-// const salt = 'Dastageer@1811';
-// const iterations = 2048;
-// const keylength = 64;
-// const result = await pbkdf2.sha512(password, salt , iterations, keylength);
-const seed = bip39.mnemonicToSeedSync(mnemonic).toString('hex')
-console.log("seed:", seed);
+const mnemonic = bip39.generateMnemonic();
+//console.log("mnemonic:", mnemonic);
 
-var hdkey = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'))
-console.log("Private:", hdkey.privateExtendedKey)
-console.log("Public:", hdkey.publicExtendedKey)
+const seed = bip39.mnemonicToSeedSync(mnemonic).toString("hex");
+//console.log("seed:", seed);
 
+var hdkey = HDKey.fromMasterSeed(Buffer.from(seed, "hex"));
+const RootPrivateKey = hdkey.privateExtendedKey.toString('hex');
+const RootPublicKey = hdkey.publicExtendedKey.toString('hex');
+// console.log("RootPrivateKey:", RootPrivateKey);
+// console.log("RootPublicKey:", RootPublicKey);
+
+const keyDerive =  hdkey.derive("m/0")
+const DerivedPublicKey = keyDerive.publicExtendedKey
+const DerivedPrivateKey = keyDerive.privateExtendedKey
+//console.log("Derived Private Key: ", DerivedPrivateKey , "\nDerived Public Key: ", DerivedPublicKey);
+
+import('tiny-secp256k1').then(ecc => BIP32Factory(ecc)).then(bip32 => {
+    let node = bip32.fromBase58(RootPublicKey)
+    let child = node.derivePath('m/0/0').__Q;
+    let publicKey = child.toString('hex');
+    pubKey = publicKey;
+    //console.log("Public Key:" ,pubKey);
+
+    const Address = bitcoin.payments.p2pkh({ pubkey: Buffer.from(pubKey, 'hex') });
+    address = Address.address;
+    //console.log("Address:", address);
+    // console.log(validate(address));
+    // console.log(getAddressInfo(address));
+})
+
+import('tiny-secp256k1').then(ecc => BIP32Factory(ecc)).then(bip32 => {
+    let node = bip32.fromBase58(RootPrivateKey)
+    let child = node.derivePath('m/0/0').__D;
+    let privateKey = child.toString('hex');
+    privKey = privateKey;
+    //console.log("privateKey:",privateKey);
+})
+
+/**----------------------------------------------Routers------------------------------------------------*/
+
+router.get("/details", (req,res) =>{
+    res.send({
+        mnemonic : mnemonic,
+        seed: seed,
+        RootPrivateKey: RootPrivateKey,
+        RootPublicKey: RootPublicKey,
+        DerivedPrivateKey: DerivedPrivateKey,
+        DerivedPublicKey: DerivedPrivateKey,
+        publicKey: pubKey,
+        privateKey: privKey
+    })
+})
+
+router.get("/generateaddress", (req,res) => {
+    res.send(address)  
+})
+
+router.get("/balance",(req,res) => {
+    res.send(`https://chain.so/api/v2/get_address_balance/BTCTEST/${address}`);
+})
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
-import express from "express";
+const express = require ('express');
+const router = require('../generatePrivateKey.js')
 
 const app = express();
 const port = 5000;
+
+app.use(express.json());
+app.use('/bit', router)
 
 app.get("/", (req, res) => {
   res.send(`Test Server ${port}`);
